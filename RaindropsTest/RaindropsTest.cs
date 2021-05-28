d810437888ae949a9b8143a4acfd26db9eddc6e8
Michael Willcock <michaelwillcock7@gmail.com>
2021-05-28 12:09:01 +0100

    Reorganised Testcases to provide more descriptive test names

--- RaindropsTest/RaindropsTest.cs
+++ RaindropsTest/RaindropsTest.cs
@@ -6,35 +6,63 @@ namespace RaindropsTest
     public class Tests
     {
         [TestCase(3, "Pling")]
-        [TestCase(5, "Plang")]
-        [TestCase(7, "Plong")]
-        [TestCase(15, "PlingPlang")]
-        [TestCase(35, "PlangPlong")]
-        [TestCase(105, "PlingPlangPlong")]
-        [TestCase(2, "2")]
-        public void BasicFunctionality(int input, string expected)
-        {
-            var actual = Program.RaindropsMethod(input);
-            Assert.That(expected, Is.EqualTo(actual));
-        }
-
-        [TestCase(28, "Plong")]
-        [TestCase(30, "PlingPlang")]
-        [TestCase(34, "34")]
-        public void BriefExampleTests(int input, string expected)
-        {
-            var actual = Program.RaindropsMethod(input);
-            Assert.That(expected, Is.EqualTo(actual));
-        }
-
         [TestCase(-3, "Pling")]
+        [TestCase(12, "Pling")]
+        public void PositiveandNegativeMultiplesOf3ReturnPling(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(5, "Plang")]
         [TestCase(-5, "Plang")]
+        [TestCase(20, "Plang")]
+        public void PositiveandNegativeMultiplesOf5ReturnPlang(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(7, "Plong")]
         [TestCase(-7, "Plong")]
+        [TestCase(28, "Plong")]
+        public void PositiveandNegativeMultiplesOf7ReturnPlong(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(15, "PlingPlang")]
         [TestCase(-15, "PlingPlang")]
+        [TestCase(30, "PlingPlang")]
+        public void PositiveandNegativeMultiplesOf3And5ReturnPlingPlang(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(35, "PlangPlong")]
         [TestCase(-35, "PlangPlong")]
+        [TestCase(70, "PlangPlong")]
+        public void PositiveandNegativeMultiplesOf5And7ReturnPlangPlong(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(21, "PlingPlong")]
+        [TestCase(-21, "PlingPlong")]
+        [TestCase(63, "PlingPlong")]
+        public void PositiveandNegativeMultiplesOf3And7ReturnPlingPlong(int input, string expected)
+        {
+            var actual = Program.RaindropsMethod(input);
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [TestCase(105, "PlingPlangPlong")]
         [TestCase(-105, "PlingPlangPlong")]
-        [TestCase(-2, "-2")]
-        public void NegativeCases(int input, string expected)
+        [TestCase(210, "PlingPlangPlong")]
+        public void PositiveandNegativeMultiplesOf3And5And7ReturnPlingPlangPlong(int input, string expected)
         {
             var actual = Program.RaindropsMethod(input);
             Assert.That(expected, Is.EqualTo(actual));
@@ -42,14 +70,14 @@ namespace RaindropsTest
 
         [TestCase(int.MaxValue, "2147483647")]
         [TestCase(int.MinValue, "-2147483648")]
-        public void ExtremeValues(int input, string expected)
+        public void ExtremeValuesWithNoMultipleReturnStringOfDigits(int input, string expected)
         {
             var actual = Program.RaindropsMethod(input);
             Assert.That(expected, Is.EqualTo(actual));
         }
 
         [TestCase(0, "PlingPlangPlong")]
-        public void ZeroTestCase(int input, string expected)
+        public void ZeroBeingAUniversalMultipleReturnsPlingPlangPlong(int input, string expected)
         {
             var actual = Program.RaindropsMethod(input);
             Assert.That(expected, Is.EqualTo(actual));
